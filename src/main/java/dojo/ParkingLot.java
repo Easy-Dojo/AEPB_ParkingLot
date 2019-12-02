878db6c7bfe1269b7dc3b7134e1bec3746b9f6bd
Liang <392425595@qq.com>
2019-12-02 20:26:47 +0800

    [parkingLot][Throw ParkingLotFullException when park a car to full parkingLot]

--- src/main/java/dojo/ParkingLot.java
+++ src/main/java/dojo/ParkingLot.java
@@ -1,13 +1,24 @@
 package dojo;
 
+import dojo.exception.ParkingLotFullException;
+
+import java.util.HashMap;
+
 public class ParkingLot {
     private int space;
+    private HashMap<Ticket, Car> parkedCars = new HashMap<>();
 
     public ParkingLot(int space) {
         this.space = space;
     }
 
-    public Ticket park(Car car) {
-        return new Ticket();
+    public Ticket park(Car car) throws ParkingLotFullException {
+        Ticket ticket = new Ticket();
+        if (parkedCars.size() >= space) {
+            throw new ParkingLotFullException();
+        }
+        parkedCars.put(ticket, car);
+
+        return ticket;
     }
 }
--- src/test/java/dojo/ParkingLotTest.java
+++ src/test/java/dojo/ParkingLotTest.java
@@ -1,17 +1,38 @@
 package dojo;
 
 
+import dojo.exception.ParkingLotFullException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ParkingLotTest {
     @Test
-    public void test_should_return_a_valid_ticket_when_park_a_car_given_parking_lot_with_space() {
+    public void test_should_return_a_valid_ticket_when_park_a_car_given_parking_lot_with_space() throws ParkingLotFullException {
         ParkingLot parkingLot = new ParkingLot(1);
         Car car = new Car();
 
         Ticket ticket = parkingLot.park(car);
 
         Assertions.assertNotNull(ticket);
+    }
+
+    @Test
+    void test_should_return_multiple_valid_ticket_when_park_multiple_car_given_parking_lot_with_enough_space() throws ParkingLotFullException {
+        ParkingLot parkingLot = new ParkingLot(3);
+
+        Ticket ticket = parkingLot.park(new Car());
+        Ticket ticket1 = parkingLot.park(new Car());
+
+        Assertions.assertNotNull(ticket);
+        Assertions.assertNotNull(ticket1);
+
+        Assertions.assertNotSame(ticket,ticket1);
+    }
+
+    @Test
+    void test_should_fail_when_park_car_given_parking_lot_full() {
+        ParkingLot parkingLot = new ParkingLot(0);
+
+        Assertions.assertThrows(ParkingLotFullException.class, () -> parkingLot.park(new Car()));
     }
 }
