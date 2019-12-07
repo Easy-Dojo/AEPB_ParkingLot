d939deddadbcd0a26ac229a86138f2885d512a15
Liang <392425595@qq.com>
2019-12-07 16:17:34 +0800

    [GraduateParkingBoy][parking multiple cars to two ParkingLot, both parking lot have enough spaces, cars will be park to first parking lot until first full]

--- src/test/java/dojo/GraduateParkingBoyTest.java
+++ src/test/java/dojo/GraduateParkingBoyTest.java
@@ -6,10 +6,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /*
-Given 有两个停车场，每个都只有适量的空车位位
-When 连续多辆车
-Then 多辆车按顺序分别停在两个停车场上，先停满第一个再停满第二个
-
 Given 有两个停车场，第一个满了，第二个有足够的空车位
 When 连续多辆车
 Then 多辆车都停在第二个停车场上
@@ -47,7 +43,7 @@ public class GraduateParkingBoyTest {
     }
 
     @Test
-    void test_should_park_into_first_when_park_multiple_cars_given_first_with_enough_space_two_is_full() throws TicketInvalidException, ParkingLotFullException {
+    void test_should_park_into_first_when_park_multiple_cars_given_first_with_enough_space_second_is_full() throws TicketInvalidException, ParkingLotFullException {
         ParkingLot parkingLot1 = new ParkingLot(3);
         ParkingLot parkingLot2 = new ParkingLot(0);
         GraduateParkingBoy graduateParkingBoy = new GraduateParkingBoy(parkingLot1, parkingLot2);
@@ -61,5 +57,26 @@ public class GraduateParkingBoyTest {
         Assertions.assertNotNull(ticketForCar2);
         Assertions.assertEquals(parkingLot1.pick(ticketForCar1), car1);
         Assertions.assertEquals(parkingLot1.pick(ticketForCar2), car2);
+    }
+
+    @Test
+    void test_should_park_both_when_park_multiple_cars_given_two_parkingLots_have_enough_space() throws TicketInvalidException, ParkingLotFullException {
+        ParkingLot parkingLot1 = new ParkingLot(2);
+        ParkingLot parkingLot2 = new ParkingLot(3);
+        GraduateParkingBoy graduateParkingBoy = new GraduateParkingBoy(parkingLot1, parkingLot2);
+
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+        Ticket ticketForCar1 = graduateParkingBoy.park(car1);
+        Ticket ticketForCar2 = graduateParkingBoy.park(car2);
+        Ticket ticketForCar3 = graduateParkingBoy.park(car3);
+
+        Assertions.assertNotNull(ticketForCar1);
+        Assertions.assertNotNull(ticketForCar2);
+        Assertions.assertNotNull(ticketForCar3);
+        Assertions.assertEquals(parkingLot1.pick(ticketForCar1), car1);
+        Assertions.assertEquals(parkingLot1.pick(ticketForCar2), car2);
+        Assertions.assertEquals(parkingLot2.pick(ticketForCar3), car3);
     }
 }
