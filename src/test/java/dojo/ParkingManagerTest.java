b7d01d8fd5695068e42eab7bef871c726331f352
Liang <392425595@qq.com>
2019-12-09 20:54:30 +0800

    [ParkingManager][ParkingManager park a car himself when parkingLot is full, throw ParkingLotFullException]

--- src/test/java/dojo/ParkingManagerTest.java
+++ src/test/java/dojo/ParkingManagerTest.java
@@ -5,6 +5,7 @@ import dojo.exception.TicketInvalidException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ParkingManagerTest {
 
@@ -16,6 +17,16 @@ class ParkingManagerTest {
         Ticket ticket = parkingManager.park(car);
 
         assertSame(car, parkingLot.pick(ticket));
+    }
+
+    @Test
+    void test_should_throw_exception_when_park_given_parking_lot_is_full_and_no_parking_boy() throws ParkingLotFullException {
+        ParkingLot parkingLot = new ParkingLot(1);
+        parkingLot.park(new Car());
+
+        ParkingManager parkingManager = new ParkingManager(parkingLot);
+
+        assertThrows(ParkingLotFullException.class, () -> parkingManager.park(new Car()));
     }
 
 }
